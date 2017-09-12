3f43acc5cbe361638a305b512915c082342b19a2
Elizabeth Ash <ash.8@wright.edu>
2017-09-12 00:10:19 -0400

    Adding hello4110.java and Hello4110.class

--- hello4110.java
+++ hello4110.java
@@ -1,5 +1,5 @@
 class Hello4110{
 	public static void main(String[] args){
-		System.out.println("Hello 4110!  Let's have a good semester!");
+		System.out.println("Hello!  Let's have a good semester!");
 	}
 }
